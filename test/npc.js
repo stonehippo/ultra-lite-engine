8e9812156dc19781d7a721ec3dab73114edd88d7
George White <stonehippo@gmail.com>
2014-12-29 09:23:23 -0500

    updated pending NPC tests

--- test/npc.js
+++ test/npc.js
@@ -10,10 +10,10 @@ describe('non-player character', function() {
 	});
 
 	describe('undecided NPC', function() {
-		it("must have a hostile reaction level")
+		it("must have an undecided reaction level")
 	});
 
 	describe('friendly NPC', function() {
-		it("must have a hostile reaction level")
+		it("must have a friendly reaction level")
 	});
 });
