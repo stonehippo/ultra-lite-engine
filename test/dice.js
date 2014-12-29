5e9d388563dbcf372302a616fa8dd84e3b0813ad
George White <stonehippo@gmail.com>
2014-12-29 09:23:44 -0500

    updated pending dice tests

--- test/dice.js
+++ test/dice.js
@@ -4,6 +4,7 @@ describe('dice', function() {
 	describe('amount', function() {
 		it("should allow a number of dice to be set")
 		it("should allow a fractional number of dice")
+		it("should require at least one die")
 	});
 
 	describe('roll', function() {
