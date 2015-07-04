e08b4e16520dfddb8cc1b2c47d27106795a21a28
George White <stonehippo@gmail.com>
2015-07-04 17:31:43 -0400

    Refined task resolution

--- test/task.js
+++ test/task.js
@@ -5,10 +5,12 @@ describe ('task', function() {
 		var task = ultralite.task.create();
 		task.should.be.an("object");
 		task.should.have.property("status");
-		task.should.have.property("difficulty");
+		task.should.have.property("baseRoll");
 		task.should.have.property("roll");
 		task.should.have.property("skill");
 		task.should.have.property("modifier");
+		task.baseRoll.should.not.be.null;
+		task.baseRoll.should.be.a('number');
 	});
 	describe('roll', function() {
 		it("should return an number between 3 and 18 inclusive", function() {
@@ -22,32 +24,55 @@ describe ('task', function() {
 	describe('resolution', function() {
 		it("must have a resolve method that sets the status and roll for the task", function()
 		{
-			var task = ultralite.task.create({'difficulty': 10});
+			var task = ultralite.task.create({'baseRoll': 10});
 			task.should.respondTo("resolve");
 			task.resolve();
 			task.roll.should.not.be.null;
 			task.status.should.not.be.null;
 		});
-		it("must always treat a roll of 3 as a critical success, regardless of base roll", function ()
+		it("must always treat a roll of 3 as a critical success, regardless of base roll and modifiers", function ()
 		{
 			var stub = sinon.stub(ultralite.task, "roll").returns(3);
-			var task = ultralite.task.create({'difficulty': 10});
+			// the task would almost always fail, since it's at total -2
+			var task = ultralite.task.create({"baseRoll": 8, "modifier": -10});
 			var taskResolution = task.resolve();
 			task.status.should.equal(ultralite.task.status.criticalSuccess);
 			task.roll.should.equal(3);
 			stub.restore();
 		});
-		it("must always treat a roll of 18 as a critical failure, regardless of base roll", function()
+		it("must always treat a roll of 18 as a critical failure, regardless of base roll and modifiers", function()
 		{
 			var stub = sinon.stub(ultralite.task, "roll").returns(18);
-			var task = ultralite.task.create({'difficulty': 10});
+			// the task would almost always succeed, since a roll of 28 or less would succeed
+			var task = ultralite.task.create({"baseRoll": 18, "modifier": 10});
 			var taskResolution = task.resolve();
 			task.status.should.equal(ultralite.task.status.criticalFailure);
 			task.roll.should.equal(18);
 			stub.restore();
 		});
-		it("must treat a roll less than or equal to the base roll as a success");
-		it("must treat a roll greater than the base roll as a failure");
-		it("must add -6 modifier to a task that requires training");
+		it("must treat a roll less than or equal to the base roll with no modifiers as a success", function()
+		{
+			var stub = sinon.stub(ultralite.task, "roll").returns(10);
+			var task = ultralite.task.create();
+			task.resolve();
+			task.roll.should.equal(10);
+			task.status.should.equal(ultralite.task.status.success);
+			stub.restore();
+		});
+		it("must treat a roll greater than the base roll with no modifiers as a failure", function()
+		{
+			var stub = sinon.stub(ultralite.task, "roll").returns(11);
+			var task = ultralite.task.create();
+			task.resolve();
+			task.roll.should.equal(11);
+			task.status.should.equal(ultralite.task.status.failure);
+			stub.restore();
+		});
+		it("must add -6 modifier to a task that requires training via a skill", function ()
+		{
+			var task = ultralite.task.create({"skill": "Knight"});
+			task.skill.should.equal("Knight");
+			task.modifier.should.equal(-6);
+		});
 	});
 });
