31e18fbae3c339bad496f96b9788af19b6ab738e
George White <stonehippo@gmail.com>
2016-09-30 18:56:15 -0400

    Character and skill WIP

--- test/skill.js
+++ test/skill.js
@@ -1,7 +1,18 @@
 describe('skill', function() {
-  it("should able to create a new skill", function() {
+  it("should return a list of available skills", function() {
+    ultralite.skill.list.should.exist;
+    ultralite.skill.list().should.eql({});
+  });
+  it("should able to create a new skill from a spec object", function() {
     ultralite.skill.create.should.exist;
     let skill = ultralite.skill.create({name: "Knight"});
     skill.should.have.property("name");
+    ultralite.skill.add(skill);
+    ultralite.skill.list().should.eql({"Knight": {name: "Knight"}});
+  });
+  it("should be able to create a new skill from a string", function() {
+    let skill = ultralite.skill.create("Ninja");
+    skill.should.have.property("name");
+      ultralite.skill.list().should.eql({"Knight": {name: "Knight"}, "Ninja": {name: "Ninja"}});
   });
 });
