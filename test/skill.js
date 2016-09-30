1f654a2eb08aeebef83a3dccfbd3ae776803dd15
George White <stonehippo@gmail.com>
2016-09-30 17:23:37 -0400

    Add basic skill implmentation

--- test/skill.js
+++ test/skill.js
@@ -1,3 +1,7 @@
 describe('skill', function() {
-  it("should able to create a new skill");
+  it("should able to create a new skill", function() {
+    ultralite.skill.create.should.exist;
+    let skill = ultralite.skill.create({name: "Knight"});
+    skill.should.have.property("name");
+  });
 });
