0a291b0741490ecf6075183fba2dc3ca560fcad6
George White <stonehippo@gmail.com>
2024-07-02 22:57:49 -0400

    Finished attribute level tests

--- test/attributeLevels.js
+++ test/attributeLevels.js
@@ -13,10 +13,30 @@ test('--- attribute module tests', assert => {
         ['weak', 'normal', 'strong', 'veryStrong']
     );
     
+    assert.deepEqual(
+        ultralite.attributeLevels.ST,
+        {
+            'weak': {'baseRoll': 8, 'hitPoints': 8, 'basicDamage': '1d6/2'},
+            'normal': {'baseRoll': 10, 'hitPoints': 10, 'basicDamage': '1d6'},
+            'strong': {'baseRoll': 14, 'hitPoints': 14, 'basicDamage': '2d6'},
+            'veryStrong': {'baseRoll': 18, 'hitPoints': 18, 'basicDamage': '3d6'}
+        }
+    );
 
     assert.deepEqual(
         Object.keys(ultralite.attributeLevels.DX),
         ['clumsy', 'normal', 'agile', 'veryAgile', 'extreme']
+    );
+
+    assert.deepEqual(
+        ultralite.attributeLevels.DX,
+        {
+            'clumsy': {'baseRoll': 8},
+            'normal': {'baseRoll': 10},
+            'agile': {'baseRoll': 12},
+            'veryAgile': {'baseRoll': 14},
+            'extreme': {'baseRoll': 16}
+        }
     );
 
     assert.deepEqual(
@@ -25,9 +45,32 @@ test('--- attribute module tests', assert => {
     );
 
     assert.deepEqual(
+        ultralite.attributeLevels.IQ,
+        {
+            'dull': {'baseRoll': 8},
+            'normal': {'baseRoll': 10},
+            'smart': {'baseRoll': 12},
+            'verySmart': {'baseRoll': 14},
+            'genius': {'baseRoll': 16}
+        }
+    );
+
+
+    assert.deepEqual(
         Object.keys(ultralite.attributeLevels.HT),
         ['sickly', 'normal', 'hardy', 'veryHardy']
     );
+
+    assert.deepEqual(
+        ultralite.attributeLevels.HT,
+        {
+            'sickly': {'baseRoll': 8},
+            'normal': {'baseRoll': 10},
+            'hardy': {'baseRoll': 14},
+            'veryHardy': {'baseRoll': 18}
+        }
+    );
+
 
     assert.end();
 });
